f995945e76342366d81cd5d5164786ae0edf0d89
jttuckman@vassar.edu <jttuckman@sp309-174.cs.vassar.edu>
2019-04-09 19:07:10 -0400

    Work in class

--- threadlab/reaction.c
+++ threadlab/reaction.c
@@ -15,6 +15,7 @@ struct reaction {
 		struct lock makingWater;
 		struct condition HSleep;
 		struct condition OSleep;
+		struct condition OSleep1;
 		struct condition OneO;
 	};
 
@@ -27,6 +28,7 @@ reaction_init(struct reaction *reaction)
 	cond_init(&reaction->HSleep);
 	cond_init(&reaction->OSleep);
 	cond_init(&reaction->OneO);
+	cond_init(&reaction->OSleep1);
 }
 
 void
@@ -35,44 +37,40 @@ reaction_h(struct reaction *reaction)
 	lock_acquire(&reaction->makingWater);
 	reaction->numH++;
 
-	if (reaction->numH == 1){ // If we only have 1 H we dont have enough and wait
-		cond_wait(&reaction->HSleep, &reaction->makingWater);
-		lock_release(&reaction->makingWater);
+
+	if (reaction->numH >= 2 && reaction->numO == 0) { // We have two Hs but no Os
+		cond_wait(&reaction->OSleep1, &reaction->makingWater); // Unlocked when a water comes
 	}
-	if (reaction-> numH >= 2 && reaction->numO == 0) { // We have two Hs but no Os
-		cond_wait(&reaction->OSleep, &reaction->makingWater); // Unlocked when a water comes
-		cond_signal(&reaction->HSleep, &reaction->makingWater); // This is locked from when we have 1 H
-		make_water();
-		reaction->numH-=2;
-		reaction->numO--;
-		lock_release(&reaction->makingWater);
-		//return;
-	}
-	if((reaction->numH >= 2) && (reaction->numO >= 1)) { // If we have all we need up front
+
+	if(reaction->numH >= 2 && reaction->numO >= 1) { // If we have all we need
 		make_water();
 		reaction->numH-=2;
 		reaction->numO--;
 		cond_signal(&reaction->OSleep, &reaction->makingWater);
 		cond_signal(&reaction->HSleep, &reaction->makingWater);
-		lock_release(&reaction->makingWater);
-		//return;
 	}
+	if (reaction->numH == 1){ // If we only have 1 H we don't have enough H and wait
+		cond_wait(&reaction->HSleep, &reaction->makingWater);
+	}
+	lock_release(&reaction->makingWater);
 }
 
 
 void
 reaction_o(struct reaction *reaction)
 {
+	lock_acquire(&reaction->makingWater);
+	
 	reaction->numO++;
-	if(reaction->numH < 2){ // Os are waiting until we have enough Hs to wake one
-		lock_acquire(&reaction->makingWater);
-		cond_wait(&reaction->OSleep, &reaction->makingWater);
-		lock_release(&reaction->makingWater);
-		}
-	if(reaction->numH >= 2 && reaction -> numO == 1){ // We have been waiting on an O
-		lock_acquire(&reaction->makingWater);
-		cond_signal(&reaction->OSleep, &reaction->makingWater);
-		lock_release(&reaction->makingWater);
+
+	if(reaction->numH >= 2){ // We have been waiting on an O
+		cond_signal(&reaction->OSleep1, &reaction->makingWater);
 	}
+
+	if(reaction->numH < 2){ // Os are waiting until we have enough Hs to wake one
+		cond_wait(&reaction->OSleep, &reaction->makingWater);
+	}	
+
+	lock_release(&reaction->makingWater);
 
 }
