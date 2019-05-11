7a39f896d18c60339ee8d5341b0f6369e48a71b9
JonahTuckman <jtuckman9@gmail.com>
2019-05-11 18:53:15 -0400

    Request header file

--- webserverlab/request.h
+++ webserverlab/request.h
@@ -1,5 +1,10 @@
 #ifndef __REQUEST_H__
 
 void request_handle(int fd);
-
+void request_error(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
+void request_read_headers(int fd);
+int request_parse_uri(char *uri, char *filename, char *cgiargs);
+void request_get_filetype(char *filename, char *filetype);
+void request_serve_dynamic(int fd, char *filename, char *cgiargs);
+void request_serve_static(int fd, char *filename, int filesize);
 #endif // __REQUEST_H__
